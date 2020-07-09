0b69128d32f2f3279901ea6111408f4cc06243d8
Kensuke Tamura <weathercock_perch@yahoo.co.jp>
2020-07-09 22:14:12 +0900

    add delay in mock response.

--- lib/model/hacker_news_service_mock.dart
+++ lib/model/hacker_news_service_mock.dart
@@ -6,6 +6,7 @@ import 'package:hacker_news_light/model/news_entry.dart';
 
 class HackerNewsServiceMock {
   Future<List<NewsEntry>> getNewsEntries(int page) async {
+    await Future.delayed(Duration(seconds: 2));
     if (page == 1) {
       return mockNews1.map((e) => NewsEntry.fromMap(e)).toList();
     } else if (page == 2) {
